e1e10ed3e386f060a555bedeb2d831d9ee8b4307
pedro_meinen <pedromeinen99@gmail.com>
2025-02-15 11:06:34 -0300

    Adicionando a logica para os endpoints da api

--- src/reserva_salas/routes/reservas.py
+++ src/reserva_salas/routes/reservas.py
@@ -1,26 +1,61 @@
 from typing import Sequence
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import select, Session
 
-from ..models import Reserva
+from ..models import Reserva, Sala
+from ..database import get_session
 
 router = APIRouter(prefix="/api/v1", tags=["Reservas"])
 
+
 @router.get("/reserva")
-async def obter_reservas() -> Sequence[Reserva]:
-    ...
+async def obter_reservas(session: Session = Depends(get_session)) -> Sequence[tuple[Reserva, Sala]]:
+    return session.exec(select(Reserva, Sala).join(Sala)).all()
+
 
 @router.get("/reserva/{id}")
-async def obter_reserva(id: int) -> Reserva:
-    ...
+async def obter_reserva(id: int, session: Session = Depends(get_session)) -> tuple[Reserva, Sala]:
+    reserva = session.exec(select(Reserva, Sala).where(Reserva.id == id).join(Sala)).first()
+
+    if not reserva:
+        raise HTTPException(404, "Reserva nao foi encontrado")
+
+    return reserva
+
 
 @router.post("/reserva")
-async def criar_reserva(reserva: Reserva) -> Reserva:
-    ...
+async def criar_reserva(reserva: Reserva, session: Session = Depends(get_session)) -> Reserva:
+    session.add(reserva)
+    session.commit()
+    session.refresh(reserva)
+
+    return reserva
+
 
 @router.put("/reserva/{id}")
-async def editar_reserva(id: int, reserva: Reserva) -> Reserva:
-    ...
+async def editar_reserva(id: int, reserva: Reserva, session: Session = Depends(get_session)) -> Reserva:
+    reserva_antiga = session.exec(select(Reserva).where(Reserva.id == id)).first()
+
+    if not reserva_antiga:
+        raise HTTPException(404, "Reserva nao foi encontrado")
+
+    reserva.id = reserva_antiga.id
+
+    session.add(reserva)
+    session.commit()
+    session.refresh(reserva)
+
+    return reserva
+
 
 @router.delete("/reserva/{id}")
-async def deletae_reserva(id: int) -> Reserva:
-    ...
+async def deletae_reserva(id: int, session: Session = Depends(get_session)) -> Reserva:
+    reserva = session.exec(select(Reserva).where(Reserva.id == id)).first()
+
+    if not reserva:
+        raise HTTPException(404, "Reserva nao foi encontrada")
+
+    session.delete(reserva)
+    session.commit()
+
+    return reserva
--- src/reserva_salas/routes/salas.py
+++ src/reserva_salas/routes/salas.py
@@ -1,26 +1,61 @@
 from typing import Sequence
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import select, Session
 
 from ..models import Sala
+from ..database import get_session
 
 router = APIRouter(prefix="/api/v1", tags=["Salas"])
 
+
 @router.get("/sala")
-async def obter_salas() -> Sequence[Sala]:
-    ...
+async def obter_salas(session: Session = Depends(get_session)) -> Sequence[Sala]:
+    return session.exec(select(Sala)).all()
+
 
 @router.get("/sala/{id}")
-async def obter_sala(id: int) -> Sala:
-    ...
+async def obter_sala(id: int, session: Session = Depends(get_session)) -> Sala:
+    sala = session.exec(select(Sala).where(Sala.id == id)).first()
+
+    if not sala:
+        raise HTTPException(404, "Sala nao foi encontrado")
+
+    return sala
+
 
 @router.post("/sala")
-async def criar_sala(sala: Sala) -> Sala:
-    ...
+async def criar_sala(sala: Sala, session: Session = Depends(get_session)) -> Sala:
+    session.add(sala)
+    session.commit()
+    session.refresh(sala)
+
+    return sala
+
 
 @router.put("/sala/{id}")
-async def editar_sala(id: int, sala: Sala) -> Sala:
-    ...
+async def editar_sala(id: int, sala: Sala, session: Session = Depends(get_session)) -> Sala:
+    sala_antiga = session.exec(select(Sala).where(Sala.id == id)).first()
+
+    if not sala_antiga:
+        raise HTTPException(404, "Sala nao foi encontrado")
+
+    sala.id = sala_antiga.id
+
+    session.add(sala)
+    session.commit()
+    session.refresh(sala)
+
+    return sala
+
 
 @router.delete("/sala/{id}")
-async def deletae_sala(id: int) -> Sala:
-    ...
+async def deletae_sala(id: int, session: Session = Depends(get_session)) -> Sala:
+    sala = session.exec(select(Sala).where(Sala.id == id)).first()
+
+    if not sala:
+        raise HTTPException(404, "Sala nao foi encontrada")
+
+    session.delete(sala)
+    session.commit()
+
+    return sala
