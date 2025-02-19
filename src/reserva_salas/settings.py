f996588659dd7c4f9f2e8af74f505b41727d20a0
Pedro Henrique Souza Meinen <pedro_meinen@sicredi.com.br>
2025-02-19 10:56:00 -0300

    Ajustando informações seguras

--- src/reserva_salas/settings.py
+++ src/reserva_salas/settings.py
@@ -1,7 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DATABASE_URI = "sqlite:///reserva_salas.db"
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
-ALGORITHM = "HS256"
-JWT_SECRET_KEY = "narscbjim@$@&^@&%^&RFghgjvbdsha"
-JWT_REFRESH_SECRET_KEY = "13ugfdfgh@#$%^@&jkl45678902"
+
+ALGORITHM = os.environ.get("ALGORITHM", "")
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "")
+JWT_REFRESH_SECRET_KEY = os.environ.get("JWT_REFRESH_SECRET_KEY", "")
