0be8626a563de4ea3793f35d8bf96b9022c001fb
Pedro Henrique Souza Meinen <pedro_meinen@sicredi.com.br>
2025-03-11 15:09:12 -0300

    Ajustando anotações de tipo

--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,9 @@
 from logging.config import fileConfig
 
 from alembic import context
-from src.database import SQLModel
 from sqlalchemy import engine_from_config, pool
+
+from src.database import SQLModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
